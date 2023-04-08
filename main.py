9befcba8eebc08e8988cc49d594a4575c51bb8a6
zls17 <ashishbalsure@gmail.com>
2023-04-08 14:02:51 +0530

    display comments

--- main.py
+++ main.py
@@ -2,12 +2,47 @@ from PyQt5.QtWidgets import (QApplication, QPushButton, QMainWindow,
                              QVBoxLayout, QWidget, QGridLayout, QLabel,
                              QLineEdit)
 from PyQt5.uic import loadUi
+from PyQt5 import QtCore, QtWidgets, QtGui
+from PyQt5.QtCore import Qt, QSize
+from PyQt5.QtGui import QFont
 from googleapiclient.discovery import build
 import pandas as pd 
 import os
 from dotenv import load_dotenv
 from googleapiclient.discovery import build
 import pandas as pd
+import csv
+
+
+class TableModel(QtCore.QAbstractTableModel):
+    def __init__(self, data):
+        super(TableModel, self).__init__()
+        self._data = data
+
+    def data(self, index, role):
+
+        if role == Qt.ItemDataRole.DisplayRole:
+            value = self._data.iloc[index.row(), index.column()]
+            return str(value)
+
+        # if role == Qt.ItemDataRole.FontRole:
+        #     return QFont("Arial", 18)
+
+    def rowCount(self, index):
+        return self._data.shape[0]
+    
+    def columnCount(self, index):
+        return self._data.shape[1]
+
+    def headerData(self, section, orientation, role):
+        if role == Qt.ItemDataRole.DisplayRole:
+            if orientation == Qt.Orientation.Horizontal:
+                return str(self._data.columns[section])
+            if  orientation == Qt.Orientation.Vertical:
+                return str(self._data.index[section])
+
+
+
 
 
 class MainWindow(QMainWindow):
@@ -45,19 +80,15 @@ class MainWindow(QMainWindow):
         self.numberCommentsEntry.textChanged.connect(self.getNumberComments)
         layout.addWidget(self.linkLabel, 0, 0)
         layout.addWidget(self.linkEntry, 0, 1)
-        # layout.addWidget(self.orLabel, 1, 0)
-        # layout.addWidget(self.channelLabel, 2, 0)
-        # layout.addWidget(self.channelEntry, 2, 1)
-        # layout.addWidget(self.videoTitle, 3, 0)
-        # layout.addWidget(self.videoTitleEntry, 3, 1)
         layout.addWidget(self.numberCommentsLabel, 1, 0)
         layout.addWidget(self.numberCommentsEntry, 1, 1)
         layout.addWidget(self.analyseButton, 2, 0)
+
         widget = QWidget()
         widget.setLayout(layout)
         self.setCentralWidget(widget)
         self.show()
-    
+
     def getNumberComments(self, number):
         self.number = int(number)
         
@@ -65,6 +96,12 @@ class MainWindow(QMainWindow):
     def getYoutubeLink(self, link):
         self.videoLink = link
     
+    def save(self):
+        with open("output.txt", 'w') as file:
+            for comment in self.comments:
+                file.write(f"{comment}\n")
+
+
     def youtubeAnalysis(self):
         load_dotenv()
         API_KEY = os.getenv("API_KEY")
@@ -78,11 +115,27 @@ class MainWindow(QMainWindow):
                 maxResults = self.number
             )
             response = request.execute()
-            comments = []
+            self.comments = []
             for i in range(self.number):
-                comments.append(response["items"][i]["snippet"]["topLevelComment"]["snippet"]["textOriginal"])
-            df = pd.DataFrame({"comments": comments})
-            print(df)
+                self.comments.append(response["items"][i]["snippet"]["topLevelComment"]["snippet"]["textOriginal"])
+            pd.options.display.max_colwidth = 100
+            self.df = pd.DataFrame(data = self.comments, columns = ["Comments"])
+            self.table = QtWidgets.QTableView()
+            self.model = TableModel(self.df)
+            self.table.setModel(self.model)
+            self.table.setColumnWidth(0, 10000)
+            self.saveButton = QPushButton("Save")
+            self.analyseButton = QPushButton("Analyse")
+            layout = QVBoxLayout()
+            layout.addWidget(self.table)
+            layout.addWidget(self.saveButton)
+            layout.addWidget(self.analyseButton)
+            widget = QWidget()
+            widget.setLayout(layout)
+            self.saveButton.pressed.connect(self.save)
+            self.setCentralWidget(widget)
+
+
 
         def url_to_id(url):
             #https://www.youtube.com/watch?v=SwSbnmqk3zY&ab_channel=techTFQ
